dbe5835c04441776224655f2eba580ed6c056c1a
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2023-06-09 11:29:41 +0900

    Create 1050.c

---
+++ C언어_기초_100제/기초_비교연산/1050.c
@@ -0,0 +1,12 @@
+// 두 정수 입력받아 비교하기2
+// == : 두 개의 값이 같은 경우 참(true)을 나타내는 정수값 1
+//      다른 경우 거짓(false)을 나타내는 정수값 0
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d", a == b);
+	return 0;
+}
