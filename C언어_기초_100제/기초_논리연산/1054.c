66475313b19ce5ec1384270f692341d7f959aa28
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2023-06-11 11:48:48 +0900

    Create 1054.c

---
+++ C언어_기초_100제/기초_논리연산/1054.c
@@ -0,0 +1,12 @@
+// 둘 다 참일 경우만 참 출력하기
+// 논리연산자 &&: 주어진 2개의 논리값이 모두 참(1) 일 때에만 1(참), 그 외의 경우에는 0(거짓)
+// AND 연산, · 으로 표시하거나 생략, 집합 기호로는 ∩(교집합, intersection)
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d", a && b);
+	return 0;
+}
