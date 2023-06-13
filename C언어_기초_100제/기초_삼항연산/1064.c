3fe75bae93307a091fe9f0ee7edbad6c8095ff70
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2023-06-13 14:20:01 +0900

    Create 1064.c

---
+++ C언어_기초_100제/기초_삼항연산/1064.c
@@ -0,0 +1,10 @@
+// 정수 3개 입력받아 가장 작은 수 출력하기
+#include <stdio.h>
+
+int main()
+{
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	printf("%d", (a < b ? a : b) < c ? (a < b ? a : b) : c);
+	return 0;
+}
