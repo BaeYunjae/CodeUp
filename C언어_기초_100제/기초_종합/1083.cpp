99f5f77fc37bce6fb3a8ae6f6df7f07d79b88da0
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2023-06-20 13:36:11 +0900

    Create 1083.cpp

---
+++ C언어_기초_100제/기초_종합/1083.cpp
@@ -0,0 +1,14 @@
+// 3 6 9 게임의 왕이 되자!
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    scanf("%d", &n);
+    for (i=1; i<=n; i++)
+    { 
+        if (i%3==0 || i%6==0 || i%9==0) printf("X ");
+        else printf("%d ", i);
+    }
+    return 0;
+}
