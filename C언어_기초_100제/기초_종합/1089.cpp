329c47dba259c17e4b6a46fd0f2a4c126e73f66c
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2023-06-23 11:00:29 +0900

    Update 1089.cpp

--- C언어_기초_100제/기초_종합/1089.cpp
+++ C언어_기초_100제/기초_종합/1089.cpp
@@ -1,5 +1,6 @@
 // 수 나열하기1
-// 등차수열: n번쨰 수 = 1번째 수 + (n번째 - 1) * 등
+// 등차수열: n번쨰 수 = 1번째 수 + (n번째 - 1) * 등차
+// 첫번째 풀이
 #include <stdio.h>
 
 int main()
@@ -7,5 +8,19 @@ int main()
     int a, d, n;
     scanf("%d %d %d", &a, &d, &n);
     printf("%d", a + (n - 1) * d);
+    return 0;
+}
+
+-----------------------------------
+// 두번째 풀이
+#include <stdio.h>
+
+int main()
+{
+    int i, j, a, d, n;
+    scanf("%d %d %d", &a, &d, &n);
+    for (i=a, j=1; ;j++, i+=d)
+        if (j == n) break;
+    printf("%d", i);
     return 0;
 }
