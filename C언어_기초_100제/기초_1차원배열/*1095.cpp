8133385e079dc037a3dd6694392ce7deb882ca0b
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2023-06-25 10:30:17 +0900

    Create *1095.cpp

---
+++ C언어_기초_100제/기초_1차원배열/*1095.cpp
@@ -0,0 +1,18 @@
+// 이상한 출석 번호 부르기3
+#include <stdio.h>
+
+int main()
+{
+    int n, i, min = 24;
+    scanf("%d", &n);
+
+    int s[n] = {};
+    for (i=0; i<n; i++) 
+    {
+        scanf("%d", &s[i]);
+        if (min > s[i])
+            min = s[i];
+    }
+    printf("%d", min);
+    return 0;
+}
