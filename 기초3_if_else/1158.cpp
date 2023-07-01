5d349a72b811a131e21234cde28d4a9495bdb4d8
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2023-07-01 10:48:59 +0900

    Create 1158.cpp

---
+++ 기초3_if_else/1158.cpp
@@ -0,0 +1,12 @@
+// 특별한 공 던지기 2
+#include <iostream>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    if ((n >= 30 and n <= 40) || (n >= 60 and n <= 70))
+        printf("win");
+    else printf("lose");
+    return 0;
+}     
