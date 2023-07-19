900ee64eafaca8236b643cd60d1e894c28209913
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2023-07-19 11:14:03 +0900

    Create 1402.c

---
+++ 기초5-1_1차원_배열/1402.c
@@ -0,0 +1,14 @@
+// 거꾸로 출력하기 3
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    scanf("%d", &n);
+    int arr[n];
+    for (i=0; i<n; i++)
+        scanf("%d", &arr[i]);
+    for (i=n-1; i>=0; i--)
+        printf("%d ", arr[i]);
+    return 0;
+}
