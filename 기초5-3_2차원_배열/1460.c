c17f33cf16bb20de58cd7554f4142158ab484290
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2023-08-10 15:35:38 +0900

    Create 1460.c

---
+++ 기초5-3_2차원_배열/1460.c
@@ -0,0 +1,22 @@
+// 2차원 배열 순서대로 채우기 1-1
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j, num = 1;
+    scanf("%d", &n);
+    int arr[n][n];
+
+    for (i=0; i<n; i++){
+        for (j=0; j<n; j++)
+            arr[i][j] = num++;
+    }
+
+    for (i=0; i<n; i++){
+        for (j=0; j<n; j++)
+            printf("%d ", arr[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
