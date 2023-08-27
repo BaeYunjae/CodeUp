7743e84e4f2db101291c8c65d0813030ced91a75
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2023-08-27 21:47:22 +0900

    Create 1473.c

---
+++ 기초5-3_2차원_배열/1473.c
@@ -0,0 +1,41 @@
+// 2차원 배열 지그재그 채우기 2-6
+#include <stdio.h>
+
+int main(){
+    int n, m, i, j;
+    scanf("%d %d", &n, &m);
+
+    int arr[n + 1][m + 1], num = n * m;
+
+    for (i=0; i<n; i++){
+        if (n % 2 == 1){
+            if (i % 2 == 0){
+                for (j=m-1; j>=0; j--)
+                    arr[i][j] = num--;
+            }
+            else{
+                for (j=0; j<m; j++)
+                    arr[i][j] = num--;
+            }
+        }
+
+        else{
+            if (i % 2 == 1){
+                for (j=m-1; j>=0; j--)
+                    arr[i][j] = num--;
+            }
+            else{
+                for (j=0; j<m; j++)
+                    arr[i][j] = num--;
+            }
+        }
+    }
+
+    for (i=0; i<n; i++){
+        for (j=0; j<m; j++)
+            printf("%d ", arr[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
